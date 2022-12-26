6d4d27ac4e8899d44af774b891ba2b06be8a1e2e
Paul van Assen <source@pvanassen.nl>
2022-12-26 13:14:44 +0100

    Fixes

--- src/main/kotlin/nl/pvanassen/led/animation/disco/Disco.kt
+++ src/main/kotlin/nl/pvanassen/led/animation/disco/Disco.kt
@@ -13,8 +13,8 @@ class Disco(private val canvas: Canvas): Animation<Any> {
     override fun getFrame(seed: Long, frame: Int, nsPerFrame: Int, helper: Any): ByteArray {
         zAxis += Math.random()
 
-        (0 .. canvas.getWidth()).forEach { x ->
-            (0 .. canvas.getHeight()).forEach { y ->
+        (0 until canvas.getWidth()).forEach { x ->
+            (0 until canvas.getHeight()).forEach { y ->
                 val hue = SimplexNoise.sumOctave(8, x.toDouble(), y.toDouble(), zAxis, 0.5, SCALE, -0.2f, 1.2f)
                 canvas.setRGB(x, y, ColorUtils.makeColorHSB(max(0.0, min(1.0, hue)).toFloat(), 1f, 1f))
             }
@@ -23,6 +23,6 @@ class Disco(private val canvas: Canvas): Animation<Any> {
     }
 
     companion object {
-        private const val SCALE = 0.04
+        private const val SCALE = 0.002
     }
 }
